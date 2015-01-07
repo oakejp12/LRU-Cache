11c0082d8b9203a0eef260f7829071d336bec8e0
Johan Oakes <oakejp12@wfu.edu>
2015-01-07 00:51:06 -0500

    Finish implementation of LRU Cache

--- LRUCache.c
+++ LRUCache.c
@@ -44,6 +44,23 @@ QNode* newQNode(unsigned pageNumber) {
     return temp;
 }
 
+
+// Utility function to create an empty Queue
+// Queue can have at most 'numberOfFrames' nodes
+Queue* createQueue(int numberfFrames) {
+    Queue* queue = (Queue *)malloc(sizeof(Queue));
+    
+    // The queue is empty
+    queue->count = 0;
+    queue->front = queue->rear = NULL;
+    
+    // Number of frames that can be stored in memory
+    queue->numberOfFrames = numberOfFrames;
+    
+    return queue;
+}
+
+
 // A utility function to create an empty Hash of given capacity
 Hash* createHash(int capacity) {
     // Allocate memory for hash
@@ -104,21 +121,102 @@ void deQueue (Queue* queue) {
 // and hash
 void Enqueue(Queue* queue, Hash* hash, unsigned pageNumber) {
     
-    // If all frames are full, remove the page at the rear
+    // If all frames are full, remove the page at the rear since we are 
+    // doing least recently used
     if (AreAllFramesFull(queue)) {
         // remove page from hash
         hash->array[queue->rear->pageNumber] = NULL;
         deQueue(queue);
     }
     
+    // Create a new node with given page number,
+    // And add the new node to the front of queue
+    QNode* temp = newQNode(pagenumber);
+    temp->next = queue->front;
     
-    
-    
-    if(isQueueEmpty(queue)){
-        
+    // If queue is empty, change both front and rear pointers
+    if(isQueueEmpty(queue))
+        queue->rear = queue->front = temp;
+    else { // Else change the front
+        queue->front->prev = temp;
+        queue->front = temp;
     }
     
+    // Add page entry to hash as well
+    hash->array[pageNumber] = temp;
+    
+    // increment number of full frames
+    queue->count++;
 }
+
+/*
+ * This function is called when a page with given 'pageNumber' is referenced
+ *  from cache (or memory). There are two cases:
+ * 1. Frame is not there in memory, we bring it in memory and add to the front of the queue
+ * 2. Frame is there in memory, we move the frame to the front of queue
+*/
+void ReferencePage(Queue* queue, Hash* hash, unsigned pageNumber) {
+    
+    QNode* reqPage = hash->array[pageNumber];
+    
+    // the page is not in cache, bring it
+    if (reqPage == NULL)
+        Enqueue(queue,hash,pageNumber);
+    else if (reqPage != queue->front) { // Page is present but not at front, change pointer
+        
+        // Unlink requested page from its current location in queue
+        reqPage->prev->next = reqPage->next;
+        if (reqPage -> next)
+            reqPage->next->prev = reqPage->prev;
+        
+        // If the requested page is rear, then change rear
+        // as this node will be move to front
+        if (reqPage == queue->rear){
+            queue->rear = reqPage->prev;
+            queue->rear->next = NULL;
+        }
+        
+        // Put the requested page before current front
+        reqPage->next = queue->front;
+        reqPage->prev = NULL;
+        
+        // Change prev of current front
+        reqPage->next->prev = reqPage;
+        
+        // Change front to requested page
+        queue->front = reqPage;
+    }
+}
+
+
+// Driver program to test above functions
+int main() {
+    
+    // Let cache hold 4 page
+    Queue* q = createQueue(4);
+    
+    // Let 10 different pages be requested
+    // PAges to be referenced are number from 0 to 9
+    Hash* hash = createHash(10);
+    
+    ReferencePage(q, hash, 1);
+    ReferencePage(q, hash, 2);
+    ReferencePage(q, hash, 3);
+    ReferencePage(q, hash, 1);
+    ReferencePage(q, hash, 4);
+    ReferencePage(q, hash, 5);
+    
+    // Let us print cache frames after the above referenced pages
+    printf("%d ", q->front->pageNumber);
+    printf("%d ", q->front->next->pageNumber);
+    printf("%d ", q->front->next->next->pageNumber);
+    printf("%d ", q->front->next->next->next->pageNumber);
+    
+    
+}
+
+
+
 
 
 
